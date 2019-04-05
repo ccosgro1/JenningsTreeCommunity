2fad130d6081b28cbdd4ba1ef662c1067fb7dda0
ccosgro1 <ccosgro1@kent.edu>
2019-04-05 10:23:36 -0400

    Made lots of edits to RDAs and for loop. For Loop: runs, need to take out a few spp and fix the main=colnames code. RDAs mostly run, need to fine tune.

--- SeedlingStats.R
+++ SeedlingStats.R
@@ -3,6 +3,8 @@
 
 library(data.table)
 library(gtools)
+library(vegan)
+library(packfor)
 ##Load the Data
 juvies=read.csv("C:/Users/Colleen/OneDrive/Documents/School/Research/Data/Trees/JuvenileMatrix.csv")
 seeds=read.csv("C:/Users/Colleen/OneDrive/Documents/School/Research/Data/Trees/SeedlingMatrix.csv")
@@ -19,23 +21,21 @@ rownames(seeds)
 for(i in 1:83) {
   tree.ringsum[i,10:42]=oldtreedata[i,10:42]+oldtreedata[i+83,10:42]+oldtreedata[i+166,10:42]  }
 rownames(tree.ringsum)=tree.ringsum[,1]
-
+tree.ringsum
 
 #Set NAs to 0 in seedling/juvenile datasets
 juvies[is.na(juvies)]<-0
 seeds[is.na(seeds)]<-0
 treedata[is.na(treedata)]<-0
 
-#Combine ecosystem type and soil variables into the seedling and juvenile datasets
-#juvies=merge(juvies,soildata)
-#seeds=merge(seeds,soildata)
-
+#to use for later analyses, after things get messed up.
+juvies1=juvies
+seeds1=seeds
 
 #Stats to do:
 #species by species way seedling to the adult and juvenile communities
   #regression of seedling abundance/height(?) to adult basal area proportion by plot
   #core v edge
-#RDAs
 
 ##ANOVAs: Juvenile abundance by ecosystem then by core/edge BY PLOT
 dim(juvies)
@@ -64,6 +64,7 @@ jcolsums=apply(juvies[,5:35],2,sum)
 plot(rank(jcolsums),jcolsums, main="Juvenile Rank Abundance")
 barplot(jcolsums)
 scolsums=apply(seeds[,5:38],2,sum)
+barplot(scolsums)
 plot(rank(scolsums),scolsums, main="Seedling Rank Abundance") #This distribution is crazy!! 
 
 
@@ -78,13 +79,17 @@ colnames(addseeds)[1]="Plot"
 colnames(addseeds)
 #put everything back together and then add the adult dataset
 juvies=merge(juvies[,c(1:4)], addseeds)
-juvies.tree=merge(juvies,treedata, by="Plot")
-juvies.tree
+juvies.tree=merge(juvies,tree.ringsum, by="Plot")
+head(juvies.tree)
 
-#Linear Regression of seedling abundance by basal area proportion
+##Linear Regression of seedling abundance by basal area proportion
+#Remove columns that don't have both juveniles and adult trees.
+juvies.tree=juvies.tree[,c(1:4,6:12,15:18,20:24,26:30,32:45,48:50,52:60,62:63,66:70,73:76,79)]
+
+
 #proportion basal area
 colnames(juvies.tree)
-tcolsums=apply(juvies.tree[,44:78],2,sum)
+tcolsums=apply(juvies.tree[,38:62],2,sum)
 propAS=(juvies.tree$ACESAC/tcolsums["ACESAC"])
 beginfor=lm(juvies.tree$ACESAC_seed~propAS)
 plot(beginfor)
@@ -92,14 +97,139 @@ summary(beginfor)
 graph=plot(juvies.tree$ACESAC_seed~propAS, ylab="Juvenile Abundance", xlab="Proportion BA")
 abline(beginfor)
 
-colnames(juvies.tree)
-for(i in 1:n){
-  propBA=(juvies.tree[,6]/tcolsums["ACERUB"])
-  regBA=lm(juvies.tree[,6]~propBA)
-  plot(regBA)
-  summary(regBA)
-  graph=plot(juvies.tree[,6]~propBA, xlab="Proportion BA", ylab="Juvenile Abundance")
-  coef(graph)
-  abline(regBA)
-}
 
+colnames(juvies.tree)
+tcolsums
+for(i in 5:29){
+  propBA=(juvies.tree[,i+33]/tcolsums[i-4])
+  regBA=lm(juvies.tree[,i]~propBA)
+  summary(regBA)
+  graph=plot(juvies.tree[,i]~propBA, xlab="Proportion BA", ylab="Juvenile Abundance", main=colnames(juvies[,i]))
+  abline(regBA)
+  }
+
+
+##RDAs
+#####JUVENILE#####
+#Hellinger Transform data
+colnames(juvies1)
+tree.rowsums=apply(juvies1[5:35],1,sum)
+juvies1[,5:35]=juvies1[,5:35]/tree.rowsums
+juvies1[,5:35]=sqrt(juvies1[,5:35])
+
+#### Forward selection using regionalized soil variables
+# First we have to merge the datasets
+#Combine ecosystem type and soil variables into the seedling and juvenile datasets
+tree.hel.soil=merge(juvies1,soildata)
+rownames(tree.hel.soil)=tree.hel.soil[,1]
+tree.hel.soil=na.exclude(tree.hel.soil)
+# Here is the global analysis. Can proceed if significant.  Use adj. R square as additional stopping criterion.
+colnames(tree.hel.soil)
+dim(tree.hel.soil)
+hel.rda.fulsoil = rda(tree.hel.soil[,5:35], tree.hel.soil[,47:71])
+anova(hel.rda.fulsoil)
+RsquareAdj(hel.rda.fulsoil)
+global.thresh=RsquareAdj(hel.rda.fulsoil)$adj.r.squared
+# Here is the forward selection command and then constructing a model with selected variables.
+hel.rda.for=forward.sel(tree.hel.soil[,5:35], tree.hel.soil[,47:71], adjR2thresh=global.thresh)
+hel.rda.for
+names(tree.hel.soil)
+hel.rda.selsoil = rda(tree.hel.soil[,5:35] ~ as.matrix(tree.hel.soil[,c(49,50,58,57,67,55)]))
+anova(hel.rda.selsoil)
+RsquareAdj(hel.rda.selsoil)
+plot(hel.rda.selsoil)
+
+#Core Plots
+tree.hel.soil=merge(juvies1,soildata)
+tree.hel.soil=tree.hel.soil[tree.hel.soil$Transect=="Core",]
+rownames(tree.hel.soil)=tree.hel.soil[,1]
+tree.hel.soil=na.exclude(tree.hel.soil)
+colnames(tree.hel.soil)
+# Here is the global analysis. Can proceed if significant.  Use adj. R square as additional stopping criterion.
+hel.rda.fulsoil = rda(tree.hel.soil[,5:35], tree.hel.soil[,c(49,50,58,57,67,55)])
+anova(hel.rda.fulsoil)
+RsquareAdj(hel.rda.fulsoil)
+global.thresh=RsquareAdj(hel.rda.fulsoil)$adj.r.squared
+
+# Here is the forward selection command and then constructing a model with selected variables.
+hel.rda.for=forward.sel(tree.hel.soil[,5:35], tree.hel.soil[,c(49,50,58,57,67,55)], adjR2thresh=global.thresh)
+colnames(tree.hel.soil)
+hel.rda.for
+names(tree.hel.soil)
+hel.rda.selsoil = rda(tree.hel.soil[,5:35] ~ as.matrix(tree.hel.soil[,c(49,50)]))
+anova(hel.rda.selsoil)
+RsquareAdj(hel.rda.selsoil)
+plot(hel.rda.selsoil)
+
+
+#Core + edge Plots
+tree.hel.soil=merge(juvies1,soildata)
+rownames(tree.hel.soil)=tree.hel.soil[,1]
+tree.hel.soil=tree.hel.soil[tree.hel.soil$Transect=="Core"|tree.hel.soil$Transect=="Edge",]
+tree.hel.soil=na.exclude(tree.hel.soil)
+colnames(tree.hel.soil)
+# Here is the global analysis. Can proceed if significant.  Use adj. R square as additional stopping criterion.
+hel.rda.fulsoil = rda(tree.hel.soil[,5:35], tree.hel.soil[,47:71])
+anova(hel.rda.fulsoil)
+RsquareAdj(hel.rda.fulsoil)
+global.thresh=RsquareAdj(hel.rda.fulsoil)$adj.r.squared
+
+# Here is the forward selection command and then constructing a model with selected variables.
+hel.rda.for=forward.sel(tree.hel.soil[,5:35], tree.hel.soil[,47:71], adjR2thresh=global.thresh)
+colnames(tree.hel.soil)
+hel.rda.for
+names(tree.hel.soil)
+hel.rda.selsoil = rda(tree.hel.soil[,5:35] ~ as.matrix(tree.hel.soil[,c(49,50,58,57)]))
+anova(hel.rda.selsoil)
+RsquareAdj(hel.rda.selsoil)
+plot(hel.rda.selsoil)
+
+
+####SEEDLING######
+#Hellinger Transform data
+colnames(seeds1)
+tree.rowsums=apply(seeds1[5:38],1,sum)
+seeds1[,5:38]=seeds1[,5:38]/tree.rowsums
+seeds1[,5:38]=sqrt(seeds1[,5:38])
+
+#### Forward selection using regionalized soil variables
+# First we have to merge the datasets
+#Combine ecosystem type and soil variables into the seedling and juvenile datasets
+tree.hel.soil=merge(seeds1,soildata)
+rownames(tree.hel.soil)=tree.hel.soil[,1]
+tree.hel.soil=na.exclude(tree.hel.soil)
+# Here is the global analysis. Can proceed if significant.  Use adj. R square as additional stopping criterion.
+colnames(tree.hel.soil)
+hel.rda.fulsoil = rda(tree.hel.soil[,5:38], tree.hel.soil[,49:73])
+anova(hel.rda.fulsoil)
+RsquareAdj(hel.rda.fulsoil)
+global.thresh=RsquareAdj(hel.rda.fulsoil)$adj.r.squared
+# Here is the forward selection command and then constructing a model with selected variables.
+#hel.rda.for=forward.sel(tree.hel.soil[,39:72], tree.hel.soil[,11:35], adjR2thresh=global.thresh)
+#hel.rda.for
+#names(tree.hel.soil)
+#hel.rda.selsoil = rda(tree.hel.soil[,39:72] ~ as.matrix(tree.hel.soil[,c(18,13,31)]))
+#anova(hel.rda.selsoil)
+#RsquareAdj(hel.rda.selsoil)
+#plot(hel.rda.selsoil)
+
+#Core Plots
+tree.hel.soil=merge(seeds1,soildata)
+tree.hel.soil=tree.hel.soil[tree.hel.soil$EP=="Core",]
+rownames(tree.hel.soil)=tree.hel.soil[,1]
+tree.hel.soil=na.exclude(tree.hel.soil)
+colnames(tree.hel.soil)
+# Here is the global analysis. Can proceed if significant.  Use adj. R square as additional stopping criterion.
+hel.rda.fulsoil = rda(tree.hel.soil[,39:72], tree.hel.soil[,11:35])
+anova(hel.rda.fulsoil)
+RsquareAdj(hel.rda.fulsoil)
+global.thresh=RsquareAdj(hel.rda.fulsoil)$adj.r.squared
+
+# Here is the forward selection command and then constructing a model with selected variables.
+hel.rda.for=forward.sel(tree.hel.soil[,39:72], tree.hel.soil[,11:35], adjR2thresh=global.thresh)
+colnames(tree.hel.soil)
+hel.rda.for
+hel.rda.selsoil = rda(tree.hel.soil[,39:72] ~ as.matrix(tree.hel.soil[,c(18,13,31)]))
+anova(hel.rda.selsoil)
+RsquareAdj(hel.rda.selsoil)
+plot(hel.rda.selsoil)
